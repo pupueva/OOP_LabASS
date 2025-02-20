208479588dc0e23210adb1b4f74022aeba9d4a95
pupueva <lubapulaeva7@gmail.com>
2025-02-20 10:46:40 +0300

    Update Task.cpp

--- OOP_Laba2/OOP_Laba2/Task.cpp
+++ OOP_Laba2/OOP_Laba2/Task.cpp
@@ -8,12 +8,12 @@ int main() {
 	int lp;
 	float *arr = new float[lp];
 	float S=0;
-	cout << "������� ����������� �������: ";
+	cout << "Ââåäèòå ðàçìåðíîñòü ìàññèâà: ";
 	cin >> l >> m;
 	float* Q = new float[l];
 	float** array = new float* [l];
 	for (int i = 0; i < l; i++) array[i] = new float[l];
-	cout << "������� �������� �������: " << endl;
+	cout << "Ââåäèòå ýëåìåíòû ìàññèâà: " << endl;
 	for (int i = 0; i < l; i++) {
 		for (int j = 0; j < m; j++) {
 			cout << "array[" << i << "][" << j << "] = ";
@@ -33,6 +33,35 @@ int main() {
 
 }
 
+	int lp;
+	float max = INT_MIN;
+	float k=0;
+	int cnt = 0;
+
+	cout << "Введите количество элементов массива: ";
+	cin>>lp;
+	float * arr = new float[lp];
+	for (int i = 0;i < lp;i++) {
+		cout << "array[" << i << "] = ";
+		cin >> arr[i];
+		if (arr[i] < 0){
+			cnt++;
+			k += arr[i];
+		}
+		if (arr[i] > max) max = arr[i];
+
+		}
+	;
+	
+	cout << "Максимальный элемент: " << max << endl;
+	if (k == 0) cout << "Отрицательных элементов в массиве нет" << endl;
+	 else cout << "Среднее арифметическое отрицательных элементов массива: " << k/cnt << endl;
+	cout << "Вывод массива в обратном порядке: ";
+	for (int i = lp - 1;i >= 0;i--) {
+		cout << arr[i] << " ";
+	}
+
+}
 
 float sort_select_one(float *arr, int lp)
 {
